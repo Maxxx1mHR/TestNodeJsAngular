16d644bc2210f347a451be142ec59fc7ce865301
Your Name <you@example.com>
2021-12-05 23:27:49 +0300

    add get single data in server

--- server/index.js
+++ server/index.js
@@ -1,67 +1,100 @@
-const express = require('express');
-const bodyparser = require('body-parser');
-const cors = require('cors');
+    const express = require('express');
+    const bodyparser = require('body-parser');
+    const cors = require('cors');
 
-const mysql = require('mysql2'); //для подключения базы данных
+    const mysql = require('mysql2'); //для подключения базы данных
 
-const app = express();
+    const app = express();
 
-app.use(cors());
-app.use(bodyparser.json());
-
-
-//database connection
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'simpledb',
-    port: 3306
-});
-
-//chech database conection
-db.connect(err => {
-    if(err) {console.log(err, 'database err');}
-    console.log('database connected..');
-})
+    app.use(cors());
+    app.use(bodyparser.json());
 
 
-// get all data
+    //database connection
+    const db = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'simpledb',
+        port: 3306
+    });
 
-app.get('/user',(req,res)=>{
-    let qr=  'SELECT * from user';
-    db.query(qr,(err,result)=>{
-        if (err)
-        {
-            console.log(err, 'errs');
-        }
-        if(result.length>0)
-        {
-            res.send({
-                message: 'all user data',
-                data: result
-            });
-        }
+    //chech database conection
+    db.connect(err => {
+        if(err) {console.log(err, 'database err');}
+        console.log('database connected..');
+    })
+
+
+    // get all data
+
+    app.get('/user',(req,res)=>{
+        let qr =  `SELECT * FROM user`;
+        db.query(qr,(err,result)=>{
+            if (err)
+            {
+                console.log(err, 'errs');
+            }
+            if(result.length>0)
+            {
+                res.send({
+                    message: 'all user data',
+                    data: result
+                });
+            }
+
+        });
 
     });
 
-});
+
+    //get single data
+
+    app.get('/user/:id',(req,res)=>{
 
 
-//get single data
+        //console.log(req.params.id,'get single data');
+        let gID = req.params.id;
+        let qr = `SELECT * FROM user WHERE id = ${gID}`;
+        db.query(qr,(err,result)=>{
+            if (err)
+            {
+                console.log(err, 'errs');
+            }
+            if(result.length>0)
+            {
+                res.send({
+                    message: 'single user data',
+                    data: result
+                });
+            }
+            else
+            {
+                res.send({
+                    message: 'data not found'
+                });
+            }
+        });
+    });
 
-app.get('/user/:id',(req,res)=>{
 
-    console.log('get single data');
-});
+    //create data
+
+    app.post('/user',(req,res)=>{
+        //console.log('postdata');
+        //console.log(req.body,'createdata');
 
 
 
 
+    });
 
-app.listen(3000,()=>{
-    console.log('server running..');
-})
+
+
+
+    app.listen(3000,()=>{
+        console.log('server running..');
+    })
 
 
 
