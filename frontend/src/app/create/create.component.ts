b1a8c8293937f8eb11324d68f6cb5220426843c5
Maxim <maksim.pudeev@bk.ru>
2021-12-09 16:16:55 +0300

    no important

--- frontend/src/app/create/create.component.ts
+++ frontend/src/app/create/create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
 
 @Component({
   selector: 'app-create',
@@ -9,7 +11,28 @@ export class CreateComponent implements OnInit {
 
   constructor() { }
 
+  errormsg: any;
+
   ngOnInit(): void {
+  }
+
+  userForm : FormGroup = new FormGroup({
+    'fullname': new FormControl('',Validators.required),
+    'email': new FormControl('', Validators.required),
+    'mobile': new FormControl('',Validators.required)
+  });
+
+  userSubmit()
+  {
+    if(this.userForm.valid)
+    {
+      console.log(this.userForm.value)
+    }
+    else
+    {
+      this.errormsg = 'all field is required';
+      //console.log('all field is required')
+    }
   }
 
 }
